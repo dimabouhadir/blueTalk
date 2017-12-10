c0059a8b491f56355e03eadbe95b6dbc2ced93f5
Dima Bou Hadir <dima.bouhadir@gmail.com>
2017-12-10 15:43:35 +0200

    edit migration

--- db/migrate/20171130193344_removenumber_of_users_from_channels.rb
+++ db/migrate/20171130193344_removenumber_of_users_from_channels.rb
@@ -1,5 +1,5 @@
 class RemovenumberOfUsersFromChannels < ActiveRecord::Migration[5.1]
   def change
-      remove_column :Channels, :number_of_users, :integer
+      remove_column :channels, :number_of_users, :integer
     end
 end
